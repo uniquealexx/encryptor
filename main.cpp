d56cc42f650e91dc446b610228ea6e4c05d29c15
uniquealexx <cutecubed1337@gmail.com>
2025-01-05 20:39:42 +0500

    remove 'if' and 'else' where is possible

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 
 namespace fs = std::filesystem;
 
+#pragma region Hash
+constexpr unsigned int Hash(const char* str) {
+    unsigned int hashValue = 5381; // initial value
+    while (*str) {
+        hashValue = (hashValue * 33) ^ *str++; // update hash value
+    }
+    return hashValue;
+}
+#pragma endregion
+
 int main(const int argc, char* argv[]) {
     std::unordered_map<std::string, std::string> options;
     std::vector<std::string> lines;
@@ -19,58 +29,69 @@ int main(const int argc, char* argv[]) {
                       << "  -xor <value>            Encrypts/decrypts the input file using XOR with the provided key\n"
                       << "  -aes <key>              Encrypts/decrypts the input file using AES with the provided key\n";
             return 0;
-        } else if (arg == "-xor") {
-            if (i + 1 < argc) {
-                options["xor"] = argv[++i];
-            } else {
-                std::cerr << "Error: -xor requires a value." << std::endl;
-                return 1;
-            }
-        } else if (arg == "-aes") {
-            if (i + 1 < argc) {
-                options["aes"] = argv[++i];
-            } else {
-                std::cerr << "Error: -aes requires a key." << std::endl;
-                return 1;
-            }
-        } else if (arg == "-i") {
-            if (i + 1 < argc) {
-                options["input"] = argv[++i];
-            } else {
-                std::cerr << "Error: -i requires an input file." << std::endl;
-                return 1;
-            }
-        } else if (arg == "-o") {
-            if (i + 1 < argc) {
-                options["output"] = argv[++i];
-            } else {
-                std::cerr << "Error: -o requires an output file." << std::endl;
-                return 1;
-            }
         } else {
-            std::cerr << "Unknown option: " << arg << std::endl;
-            return 1;
+            switch (Hash(arg.c_str())) {
+                case Hash("-xor"):
+                    if (i + 1 < argc) {
+                        options["xor"] = argv[++i];
+                    } else {
+                        std::cerr << "Error: -xor requires a value." << std::endl;
+                        exit(1);
+                    }
+                    break;
+                case Hash("-aes"):
+                    if (i + 1 < argc) {
+                        options["aes"] = argv[++i];
+                    } else {
+                        std::cerr << "Error: -aes requires a key." << std::endl;
+                        exit(1);
+                    }
+                    break;
+                case Hash("-i"):
+                    if (i + 1 < argc) {
+                        options["input"] = argv[++i];
+                    } else {
+                        std::cerr << "Error: -i requires an input file." << std::endl;
+                        exit(1);
+                    }
+                    break;
+                case Hash("-o"):
+                    if (i + 1 < argc) {
+                        options["output"] = argv[++i];
+                    } else {
+                        std::cerr << "Error: -o requires an output file." << std::endl;
+                        exit(1);
+                    }
+                    break;
+                default:
+                    std::cerr << "Error: Unknown option " << arg << std::endl;
+                    exit(1);
+            }
         }
     }
 #pragma endregion
 
 #pragma region Input Files
-    if (options.contains("input")) {
-        if (std::ifstream inputFile(options["input"]); inputFile.good()) {
-            inputFile.seekg(0, std::ios::end);
-            if (std::streampos fileSize = inputFile.tellg(); fileSize == 0) {
-                std::cerr << "Error: Input file '" << options["input"] << "' is empty." << std::endl;
-                return 1;
-            }
-            inputFile.seekg(0, std::ios::beg); // wayback
-            std::string line;
-            while (std::getline(inputFile, line)) {
-                lines.push_back(line);
-            }
-        } else {
-            std::cerr << "Error: Input file '" << options["input"] << "' does not exist or cannot be opened." << std::endl;
-            return 1;
-        }
+    if (!options.contains("input")) {
+        return 1; // no input file specified, nothing to do
+    }
+
+    std::ifstream inputFile(options["input"]);
+    if (!inputFile.good()) {
+        std::cerr << "Error: Input file '" << options["input"] << "' does not exist or cannot be opened." << std::endl;
+        return 1;
+    }
+
+    inputFile.seekg(0, std::ios::end);
+    if (std::streampos fileSize = inputFile.tellg(); fileSize == 0) {
+        std::cerr << "Error: Input file '" << options["input"] << "' is empty." << std::endl;
+        return 1;
+    }
+
+    inputFile.seekg(0, std::ios::beg); // reset to the beginning of the file
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        lines.push_back(line);
     }
 #pragma endregion
 
@@ -79,8 +100,8 @@ int main(const int argc, char* argv[]) {
         std::cout << "XOR enabled with value: " << options["xor"] << std::endl;
         Encryptor xorEncryptor(std::make_unique<XorString>());
 
-        for (const auto& line : lines) {
-            encryptedLines.push_back(xorEncryptor.Encrypt(line, options["xor"]));
+        for (const auto& data : lines) {
+            encryptedLines.push_back(xorEncryptor.Encrypt(data, options["xor"]));
         }
     }
 
@@ -90,23 +111,26 @@ int main(const int argc, char* argv[]) {
 #pragma endregion
 
 #pragma region Output Files
-    if (options.contains("output")) {
-        std::ofstream outputFile(options["output"]);
-        if (!outputFile) {
-            std::cerr << "Error: Output file '" << options["output"] << "' cannot be created or opened." << std::endl;
-            return 1;
-        }
-
-        if (!encryptedLines.empty()) {
-            for (const auto& encryptedLine : encryptedLines) {
-                outputFile << encryptedLine << std::endl;
-            }
-            std::cout << "Encrypted data written to '" << options["output"] << "'." << std::endl;
-        } else {
-            std::cerr << "Error: No data to write to output file." << std::endl;
-            return 1;
-        }
+    if (!options.contains("output")) {
+        return 1; // exit if "output" option is not present
     }
+
+    std::ofstream outputFile(options["output"]);
+    if (!outputFile) {
+        std::cerr << "Error: Output file '" << options["output"] << "' cannot be created or opened." << std::endl;
+        return 1;
+    }
+
+    if (encryptedLines.empty()) {
+        std::cerr << "Error: No data to write to output file." << std::endl;
+        return 1;
+    }
+
+    for (const auto& encryptedLine : encryptedLines) {
+        outputFile << encryptedLine << std::endl;
+    }
+
+    std::cout << "Encrypted data written to '" << options["output"] << "'." << std::endl;
 #pragma endregion
 
     std::cout << "Input file: " << options["input"] << std::endl;
